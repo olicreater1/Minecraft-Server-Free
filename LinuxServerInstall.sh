d2aa8075b3c30b7ac7ab2bf8e27d5d99eb99e6da
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2024-11-15 14:15:04 -0500

    Update and rename LinuxServer.sh to LinuxServerInstall.sh

--- LinuxServerInstall.sh
+++ LinuxServerInstall.sh
@@ -5,3 +5,7 @@ cd server
 wget $link
 filename=$(find . -maxdepth 1 -name '.jar*')
 java -jar $filename -nogui
+echo eula=true > eula.txt
+touch run.sh
+echo java -jar $filename -nogui > run.sh
+rm LinuxServerInstall.sh

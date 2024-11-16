527f95e9bfa12b3f5b7a0cd96c65f6b359c9d2ef
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2024-11-16 08:37:00 -0800

    Update LinuxServerInstall.sh

--- LinuxServerInstall.sh
+++ LinuxServerInstall.sh
@@ -6,6 +6,5 @@ wget $link
 filename=$(find . -maxdepth 1 -name '.jar*')
 java -jar $filename -nogui
 echo eula=true > eula.txt
-touch run.sh
 echo java -jar $filename -nogui > run.sh
 rm LinuxServerInstall.sh

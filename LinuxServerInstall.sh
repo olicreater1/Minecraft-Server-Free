6393c4725fe19faa8b6b7f3e58ea7b09cfc50f57
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2024-11-16 08:39:08 -0800

    Update LinuxServerInstall.sh

--- LinuxServerInstall.sh
+++ LinuxServerInstall.sh
@@ -1,7 +1,7 @@
 echo Paste Link For Server Files
 read link
-mkdir server-$link
-cd server-$link
+mkdir server
+cd server
 wget $link
 filename=$(find . -maxdepth 1 -name '.jar*')
 java -jar $filename -nogui

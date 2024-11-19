d3ae4d8f59ea1a82db96bc443e371c7082f60c61
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2024-11-19 13:29:15 -0800

    Update LinuxServerInstall.sh

--- LinuxServerInstall.sh
+++ LinuxServerInstall.sh
@@ -3,10 +3,17 @@ echo "Paste Link For Server Files:"
 read link
 mkdir -p server
 cd server
-wget "$link"
-filename=$(find . -maxdepth 1 -name '*.jar')
-java -jar "$filename" -nogui
-echo "eula=true" > eula.txt
-echo "java -jar $filename -nogui" > run.sh
-chmod +x run.sh
-rm ../LinuxServerInstall.sh
+wget -O server.jar "$link"
+
+# Check if the downloaded file is actually a JAR file
+if file server.jar | grep -q 'Java archive'; then
+    java -jar server.jar -nogui
+    echo "eula=true" > eula.txt
+    echo "java -jar server.jar -nogui" > run.sh
+    chmod +x run.sh
+    rm ../LinuxServerInstall.sh
+else
+    echo "Error: The downloaded file is not a valid JAR file."
+    rm server.jar
+    exit 1
+fi
